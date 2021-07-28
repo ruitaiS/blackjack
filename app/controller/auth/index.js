fd64fcaddac1145e365a3bff9919296c24ac1963
Terry Lee <slsk0219@gmail.com>
2021-07-28 09:55:52 -0700

    user authentication

--- app/controller/auth/index.js
+++ app/controller/auth/index.js
@@ -1,15 +1,52 @@
 import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+import User from "../../models/user.js"
 
 export const signUp = async (req, res) => {
+  const { username, password } = req.body
+
   try {
+    // find existing user
+    const existingUser = await User.find({ username })
+
+    // if user doesn't exist creat new user
+    if (!existingUser.length) {
+      const hashedPassword = await bcrypt.hash(password, 12)
+      const newUser = await User.create({
+        username,
+        password: hashedPassword
+      })
+
+      // create token
+      const token = jwt.sign({ username: newUser.username, id: newUser._id }, "test1234")
+
+      res.status(200).json({ token })
+    }
+    res.status(400).json({ message: "Something wrong with username/password" })
   } catch (err) {
-    console.log(err)
+    res.status(400).json({ message: "Something wrong with username/password" })
   }
 }
 
 export const signIn = async (req, res) => {
+  const { username, password } = req.body
+
   try {
+    // find existing user
+    const existingUser = await User.find({ username })
+    if (!existingUser.length) {
+      return res.status(400).json({ message: "user doesn't exist" })
+    } else {
+      // compare password to see if it's valid
+      const isValid = await bcrypt.compare(password, existingUser[0].password)
+      if (!isValid) return res.status(401).json({ message: "your password doesn't match" })
+
+      // create token
+      const token = jwt.sign({ username: existingUser.username, id: existingUser._id }, "test1234")
+
+      return res.status(200).json({ token })
+    }
   } catch (err) {
-    console.log(err)
+    return res.status(401).json({ message: "something went wrong" })
   }
 }
--- app/server.js
+++ app/server.js
@@ -1,16 +1,28 @@
 import express from "express"
 import cors from "cors"
 import authRoutes from "./routes/auth/index.js"
+import bodyParser from "body-parser"
+import mongoose from "mongoose"
 
 const app = express()
 
 app.use(express.json())
+app.use(bodyParser())
 app.use(cors())
 
 app.use("/auth", authRoutes)
 
 app.get("/", (request, response) => response.send("welcome to blackjack api"))
 
-app.listen(4000, (res, req) => {
-  console.log("app started at port 4000")
-})
+mongoose
+  .connect(
+    "mongodb+srv://blackjack:pDCD6BWZxbMJgN8i@cluster0.2hjrp.mongodb.net/blackjack?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
+  )
+  .then(() => {
+    app.listen(4000, (res, req) => {
+      console.log("app started at port 4000")
+    })
+  })
+
+mongoose.set("useFindAndModify", false)
